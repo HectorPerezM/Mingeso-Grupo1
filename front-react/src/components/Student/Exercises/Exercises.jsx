cb9383883c4a077b6aaba2c6c3ce7a1f1b9f0003
psykosoldierz <juan.depablo@usach.cl>
2018-08-22 13:01:55 -0300

    adding styles

--- front-react/src/components/Student/Exercises/Exercises.jsx
+++ front-react/src/components/Student/Exercises/Exercises.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import NavLink from 'react-router-dom/NavLink';
+import {NavLink, Link} from 'react-router-dom';
 import axios from 'axios';
 import { Grid, Row, Col, ButtonToolbar, Button, Modal } from 'react-bootstrap';
 
@@ -8,10 +8,10 @@ import './Exercises.css';
 class Exercises extends Component{
     constructor(props) {
         super(props);
-  
+
         this.handleShow = this.handleShow.bind(this);
         this.handleHide = this.handleHide.bind(this);
-  
+
         this.state = {
             problems: [{
                 problemId: 0,
@@ -36,39 +36,38 @@ class Exercises extends Component{
         });
         this.setState({ problems: newProblems });
     }
-  
+
     handleHide = (i) => (e) => {
         const newProblems = this.state.problems.map((problem, j) => {
             if (i !== j) return problem;
-            
+
             return { ...problem, show: false,  };
         });
 
         this.setState({ problems: newProblems });
     }
-  
-  
+
+
     componentDidMount() {
-        axios.get('http://165.227.48.161:8082/problems')
+        axios.get('http://206.189.181.197:8082/problems')
              .then(res => {
                 const problems = res.data;
                 this.setState({problems});
                 console.log(problems.length);
-                const asd = {show: false};
-                for (var i = 0; i < problems.length; i++) {
-                    problems[i] = {...problems[i], ...asd };
-                }
+                //for (var i = 0; i < problems.length; i++) {
+                //    problems[i] = {...problems[i], ...asd };
+                //}
                 this.setState(problems);
                 console.log(this.state.problems);
             })
     }
-    
+
     render(){
         return(
             <div className="container">
-                
+
                 <h1 className="title">Ejercicios propuestos</h1>
-                
+
                 <div className="body">
 
                 <Row>
@@ -88,16 +87,18 @@ class Exercises extends Component{
                                     <td className="td-name">
                                         {i+1}
                                     </td>
-                                    
+
                                     <td>
                                     {item.problemTitle}
                                     </td>
-                                    
+
                                     <td>
                                         <ButtonToolbar className="td-btn-view">
                                             <Button className="btn-view" onClick={this.handleShow(i)}>
+                                            <i class="far fa-eye"></i>
+
                                         </Button>
-                                    
+
                                         <Modal
                                             show={this.state.problems[i].show}
                                             onHide={this.handleHide(i)}
@@ -109,14 +110,14 @@ class Exercises extends Component{
                                                     {this.problemTitle}
                                                 </Modal.Title>
                                             </Modal.Header>
-                                            
+
                                             <Modal.Body>
                                                 <h4>Descripción</h4>
                                                 <p>
                                                     {item.problemStatement}
                                                 </p>
                                             </Modal.Body>
-                                            
+
                                             <Modal.Footer>
                                                 <Button onClick={this.handleHide(i)}>Cerrar</Button>
                                             </Modal.Footer>
@@ -125,7 +126,9 @@ class Exercises extends Component{
                                     </td>
                                     <td className="td-actions">
                                     <div className="td-btn-solve">
-                                        <NavLink className="btn-solve" to={'/exercises/' + item.problemId}>._._.</NavLink>
+                                      <Link className="ojo" to={'/exercises/' + item.problemId}>
+                                      <i class="fas fa-keyboard"></i>
+                                      </Link>
                                     </div>
                                     </td>
                                 </tr>
@@ -133,7 +136,7 @@ class Exercises extends Component{
                             </tbody>
                         </table>
                     </Col>
-                </Row>    
+                </Row>
                 </div>
             </div>
         );
